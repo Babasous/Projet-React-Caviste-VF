0e5979e08c079c0b6b748a2b72a530e1b231c50a
Alain <albulaya@gmail.com>
2021-12-02 00:36:59 +0100

    Structure de base

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import axios from 'axios';
+const borderTable = {
+  border: '2px solid red'
+}
+
+function FilterableProductTable() {
+
+  const [data, setData] = useState([]);
+  // useEffect est similaire à "componentDidMount" et "componentDidUpdate"
+  useEffect(() => {
+    fetch('https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setData(result);
+        }
+
+      )
+  }, [])
+  console.log(data);
+  return <h1 style={borderTable}>{data.map(item => (
+    <p key={item.id}>{item.region} {item.name} {item.price} {item.country}</p>
+  ))}</h1>
+}
+
+//export default FilterableProductTable;
+ReactDOM.render(<FilterableProductTable />, document.getElementById('root'));
